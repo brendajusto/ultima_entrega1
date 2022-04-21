d07f64daa5024ff1ed0ab66bc4d7653460d57d19
[brendajusto] <[brendadiehl@icloud.com]>
2022-04-21 12:02:32 -0300

    segunda tag

--- imagem/mercado.java
+++ imagem/mercado.java
@@ -5,5 +5,25 @@ public class Mercado{
 
     public float getpreco(){
         return preco;
+
+        public int getqte(){
+        return qte;
+    }
+
+    public float getTotal(){
+        return total;
+    }
+    public void setPreco(float preco){
+        this.preco = preco;
+    }
+
+    public void setqte(int qte){
+        this.qte = qte;
+    }
+
+    public void setTotal(float total){
+        this.total = total;
+    }
+
     }
 
